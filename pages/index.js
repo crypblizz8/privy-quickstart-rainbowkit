17667af0fe958adfa0d4fc5fb7295405786a7f03
C <45455218+crypblizz8@users.noreply.github.com>
2022-07-28 22:46:52 +0200

    init rainbowkit
    Rm hydration error with initial authentication

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { PrivyClient, SiweSession } from "@privy-io/privy-browser";
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
 
 // Initialize the Privy client.
 const provider = typeof window !== "undefined" ? window.ethereum : null;
@@ -13,27 +15,44 @@ const client = new PrivyClient({
 });
 
 export default function Home() {
+  // Use Wagmi Address instead of settingState through privy client.
+  const { address: ethAddress, isConnected } = useAccount();
+
   // Use React's useState hook to keep track of the signed in Ethereum address.
   const [address, setAddress] = useState(null);
   const [firstName, setFirstName] = useState("");
   const [dateOfBirth, setDateOfBirth] = useState("");
   const [favoriteColor, setFavoriteColor] = useState("");
 
-  // Connect to a MetaMask wallet.
-  const connectToWallet = async () => {
+  // React States for authentication status + if data has been pushed to Privy.
+  // const [authenticatedwithPrivy, setAuthenticatedwithPrivy] = useState(false);
+  const [savedData, setSavedData] = useState(false);
+
+  // Moved all useEffects to top for readability.
+  // Initial Auth.
+  useEffect(() => {
+    if (isConnected) {
+      setAddress(ethAddress);
+    }
+  }, [address, ethAddress, isConnected]);
+
+  // Get the user data from Privy whenever the wallet address is set.
+  // useEffect(() => {
+  //   if (isConnected) getUserData();
+  // }, []);
+
+  // Set background to user's favorite color.
+  useEffect(() => {
+    if (!favoriteColor) return;
+    document.body.style = `background: ${favoriteColor};`;
+  }, [favoriteColor]);
+
+  // Authenticate with Privy after RainbowKit has connected to the Ethereum network.
+  const authWithPrivy = async () => {
     try {
-      const { ethereum } = window;
-
-      if (!ethereum) {
-        alert("Please install MetaMask for this demo: https://metamask.io/");
-        return;
-      }
-
-      if (!(await session.isAuthenticated())) {
+      if (!(await session.isAuthenticated()) && address) {
         await session.authenticate();
       }
-      const address = await session.address();
-      setAddress(address);
     } catch (error) {
       console.error(error);
     }
@@ -58,22 +77,27 @@ export default function Home() {
     setFirstName(name.text());
     setDateOfBirth(birthday.text());
     setFavoriteColor(color.text());
+    if (name && birthday && color) {
+      console.log("Successfully wrote user data to Privy.");
+      setSavedData(true);
+    }
   };
 
-  // Update address if page is refreshed.
-  const updateAddress = async () => {
-    const address = await session.address();
-    setAddress(address);
-  };
-  useEffect(() => {
-    updateAddress();
-  }, []);
+  // Commented out due to Rainbowkit already tracking...
+  // Can remove later if no bugs.
 
-  // Get user data from Privy.
+  // const updateAddress = async () => {
+  //   const address = await session.address();
+  //   // setAddress(address);
+  // };
+  // useEffect(() => {
+  //   // updateAddress();
+  // }, []);
+
+  // Get user data from Privy for initial state if user has already written to the DB.
   const getUserData = async () => {
     try {
       if (!address) return;
-
       // Fetch user's name and favorite color from Privy
       const [firstName, dateOfBirth, favoriteColor] = await client.get(
         address,
@@ -87,68 +111,82 @@ export default function Home() {
     }
   };
 
-  // Get the user data from Privy whenever the wallet address is set.
-  useEffect(() => {
-    getUserData();
-  }, [address]);
-
-  // Set background to user's favorite color.
-  useEffect(() => {
-    if (!favoriteColor) return;
-    document.body.style = `background: ${favoriteColor};`;
-  }, [favoriteColor]);
-
-  return (
-    <>
-      <Head>
+  const headerContent = (
+    <div>
+      <>
         <title>Privy Quickstart</title>
-      </Head>
-      {!address && (
-        <>
-          <div>To get started, connect with MetaMask!</div>
-          <button onClick={connectToWallet}>Connect Wallet</button>
-        </>
-      )}
-      {address && (
-        <div className="container">
-          <h1>
-            Hey {firstName ? firstName : address.substring(0, 5) + "..."} 👋
-          </h1>
-          <div>
-            <div className="inputForm">
-              <label htmlFor="name">Name</label>
-              <input
-                id="name"
-                onChange={(event) => {
-                  setFirstName(event.target.value);
-                }}
-                value={firstName}
-                placeholder={address.substring(0, 5) + "..."}
-              />
-              <label htmlFor="dob">Date of Birth</label>
-              <input
-                id="Date Of Birth"
-                onChange={(event) => {
-                  setDateOfBirth(event.target.value);
-                }}
-                value={dateOfBirth}
-              />
-              <label htmlFor="color">Favorite Color</label>
-              <input
-                onChange={(event) => {
-                  setFavoriteColor(event.target.value);
-                }}
-                value={favoriteColor}
-              />
-            </div>
-          </div>
-          <div>
-            <button style={{ fontSize: "1.6rem" }} onClick={putUserData}>
-              Save with Privy
-            </button>
+      </>
+      <div className="walletHeader">
+        <ConnectButton />
+      </div>
+    </div>
+  );
+
+  const authContent = () => {
+    return (
+      <div>
+        <p> Connect with the wallet first :) </p>
+        <button style={{ borderRadius: 8 }} onClick={authWithPrivy}>
+          {!address ? "Auth with Privy" : "Authed ✅"}
+        </button>
+      </div>
+    );
+  };
+
+  const connectedContent = () => {
+    return (
+      <div>
+        <p>
+          {address || firstName
+            ? `Hey ${
+                firstName ? firstName : address.substring(0, 5) + "..."
+              } 👋`
+            : `Connect with the wallet first :)`}
+        </p>
+        <div>
+          <div className="inputForm">
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              onChange={(event) => {
+                setFirstName(event.target.value);
+              }}
+              value={firstName}
+              placeholder={address ? address.substring(0, 5) + "..." : null}
+            />
+            <label htmlFor="dob">Date of Birth</label>
+            <input
+              id="Date Of Birth"
+              onChange={(event) => {
+                setDateOfBirth(event.target.value);
+              }}
+              value={dateOfBirth}
+            />
+            <label htmlFor="color">Favorite Color</label>
+            <input
+              onChange={(event) => {
+                setFavoriteColor(event.target.value);
+              }}
+              value={favoriteColor}
+            />
           </div>
         </div>
-      )}
-    </>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <button style={{ borderRadius: 8 }} onClick={putUserData}>
+            {!savedData ? "Save with Privy" : "Pushed data to Privy ✅"}
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div>{headerContent}</div>
+      <div className="container">
+        <h1 style={{ margin: 0 }}>Privy Quickstart</h1>
+        <div>{connectedContent()}</div>
+      </div>
+    </div>
   );
 }
